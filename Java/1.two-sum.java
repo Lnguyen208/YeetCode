1f706129878d91344f4d0127a7e71d5f8ed9f263
Lnguyen208 <LNGUYEN208@STUDENT.GSU.EDU>
2022-10-02 18:20:55 -0400

    Solution to problem 1

---
+++ Java/1.two-sum.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        // brute force appraoch
+        // nested for loops to check every combination
+        // O(n^2) time O(k) space
+        // sort + binary search approach
+        // sort array, iterate over every position
+        // use binary search to find operand
+        // O(nlog(n) + nlog(n)) time O(k) space
+        // hashmap approach
+        // iterate over nums, construct hashmap of (absolute difference, index)
+        // check hashmap at every iteration for matching pair
+        // O(n) time O(n) space
+
+        HashMap <Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(nums[i])) {
+                return new int[] {hashMap.get(nums[i]), i};
+            }
+            // get delta, put in hashmap
+            hashMap.put(target - nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
+// @lc code=end
+
