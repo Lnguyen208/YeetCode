cc90203c0394e0775a6815e4044eaa86af4c7a34
Lnguyen208 <LNGUYEN208@STUDENT.GSU.EDU>
2022-10-18 23:54:13 -0400

    Solution to LeetCode 121

---
+++ Java/121.best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=121 lang=java
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+import java.util.*;
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        // Brute Force Approach:
+        // nested loop, compile list of differences
+        // return greatest difference
+        // O(n^2) time, O(n^2) space
+        // Two-Pointer, Sliding Window:
+        // buy pointer, sell pointer is ahead of buy pointer
+        // computes difference between pointers and compares
+        // to current max profit
+        // O(n) time, O(k) space
+
+        int buy = 0;
+        int sell = 1;
+        int maxProfit = 0;
+        
+        while (sell < prices.length) {
+            if (prices[buy] < prices[sell]) {
+                int profit = prices[sell] - prices[buy];
+                maxProfit = (profit > maxProfit) ? profit : maxProfit;
+            }
+            else {
+                buy = sell;
+            }
+            sell++;
+        }
+        return maxProfit;
+    }
+}
+// @lc code=end
