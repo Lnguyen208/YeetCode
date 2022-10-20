1f7e230fb123abb787dcbf629cd73f25cf6fbe22
Lnguyen208 <LNGUYEN208@STUDENT.GSU.EDU>
2022-10-19 21:39:28 -0400

    Solution to LeetCode 509, Using DP

--- Java/509.fibonacci-number.java
+++ Java/509.fibonacci-number.java
@@ -1,5 +1,3 @@
-import javax.lang.model.util.ElementScanner6;
-
 /*
  * @lc app=leetcode id=509 lang=java
  *
@@ -9,19 +7,28 @@ import javax.lang.model.util.ElementScanner6;
 // @lc code=start
 class Solution {
     public int fib(int n) {
-    // Brute Force: 
-    // Interate using for loop to compute
-    // Recursion: 
-    // Base case-- n = 1 or n = 2
-    // O(2^n) time, O(call stack?) space
-        if (n == 0) return 0;
-        if (n == 1)
-            return 1;
-        if (n == 2)
-            return 1;
-        else   
-            return fib(n-1) + fib(n-2);
+        // Brute Force:
+        // Interate using for loop to compute
+        // Recursion:
+        // Base case-- n = 1 or n = 2
+        // O(2^n) time, O(call stack?) space
+        // 1-D Dynamic Programming:
+        // Tabulation:
+        // O(n) time, O(k) space
+        if (n == 0)
+            return 0;
+
+        int pos1 = 1;
+        int pos2 = 1;
+        int counter = 2;
+
+        while (counter < n) {
+            int temp = pos2;
+            pos2 += pos1;
+            pos1 = temp;
+            counter++;
+        }
+        return pos2;
     }
 }
 // @lc code=end
-
