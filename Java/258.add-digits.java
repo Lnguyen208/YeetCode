dee188665046108f4d0f32fbefcb9dad8a26eed0
Lnguyen208 <LNGUYEN208@STUDENT.GSU.EDU>
2022-11-14 22:42:53 -0500

    Updated LeetCode 258 w/ O(1) soln

--- Java/258.add-digits.java
+++ Java/258.add-digits.java
@@ -7,20 +7,20 @@
 // @lc code=start
 class Solution {
     public int addDigits(int num) {
-        // Modulo Approach:
+        // Modulo/Divide Loop Approach:
         // Double loops, outer loops until num is 1 digit
         // inner loop loops modulo and add to running sum
         // O(n^2) time, O(k) space
-
-        while (num > 9) {
-            int temp = 0;
-            while (num != 0) {
-                temp += num % 10;
-                num /= 10;
-            }
-            num = temp;
-        }
-        return num;
+        // Modulo Math Approach:
+        // Use result of num modulo 9 to calculate remainder since
+        // the output will be a value between 0-9 and adding digit
+        // cycle is based on decimal system
+        // O(k) time, O(k) space
+        if (num == 0)
+            return 0;
+        else if (num % 9 == 0)
+            return 9;
+        return num % 9;
     }
 }
 // @lc code=end
