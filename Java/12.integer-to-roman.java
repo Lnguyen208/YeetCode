f503a220ee5d6906526e3d648ae69433ae8842cc
Lnguyen208 <LNGUYEN208@STUDENT.GSU.EDU>
2022-11-29 22:40:43 -0500

    Initial Soln LeetCode 12

---
+++ Java/12.integer-to-roman.java
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode id=12 lang=java
+ *
+ * [12] Integer to Roman
+ */
+
+// @lc code=start
+import java.util.*;
+
+class Solution {
+    public String intToRoman(int num) {
+        // Dictionary and Modulo Approach:
+        // Creat a HashMap dictionary to track number of modulo done,
+        // actual modulo value, and Character
+        // Modulo input integer and match with appropriate Character(s)
+        HashMap<Integer, HashMap<Integer, Character>> dict = new HashMap<Integer, HashMap<Integer, Character>>();
+        dict.put(0, new HashMap<Integer, Character>());
+        dict.get(0).put(1, 'I');
+        dict.get(0).put(5, 'V');
+
+        dict.put(1, new HashMap<Integer, Character>());
+        dict.get(1).put(1, 'X');
+        dict.get(1).put(5, 'L');
+
+        dict.put(2, new HashMap<Integer, Character>());
+        dict.get(2).put(1, 'C');
+        dict.get(2).put(5, 'D');
+
+        dict.put(3, new HashMap<Integer, Character>());
+        dict.get(3).put(1, 'M');
+
+        StringBuilder soln = new StringBuilder();
+        int modNum = 0;
+        int mod = 0;
+
+        while (num > 0) {
+            mod = num % 10;
+            if (mod == 4) {
+                soln.insert(0, dict.get(modNum).get(5));
+                soln.insert(0, dict.get(modNum).get(1));
+            }
+            else if (mod == 9) {
+                soln.insert(0, dict.get(modNum + 1).get(1));
+                soln.insert(0, dict.get(modNum).get(1));
+            }
+            else {
+                boolean flag = mod > 4;
+                if (flag) {
+                    mod -= 5;
+                }
+                while (mod > 0) {
+                    soln.insert(0, dict.get(modNum).get(1));
+                    mod--;
+                }
+                if (flag) {
+                    soln.insert(0, dict.get(modNum).get(5));
+                }
+            }
+            modNum++;
+            num/=10;
+        }
+        return soln.toString();
+    }
+}
+// @lc code=end
